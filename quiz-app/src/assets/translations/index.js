fa7267cc4dc66b552d306101e92f0c01cacd5cb2
Jks Liu <chinatianma@gmail.com>
2021-07-27 22:58:02 +0800

    Quiz App Chinese Translation (#251)
    * Quiz app zh-cn: Class 1 ~ 5
    
    * Quiz app zh-cn: Class 6 ~ 10
    
    * Quiz app zh-cn: Class 11 ~ 15
    
    * Quiz app zh-cn: Class 16 ~ 20
    
    * Quiz app zh-cn: Class 21 ~ 24
    
    * Make course names same with https://github.com/microsoft/IoT-For-Beginners/blob/main/translations/README.zh-cn.md
    
    * Quiz app zh-cn: Final check
    
    * Quiz app zh-cn: Add zh-cn language to quiz app
    
    * Quiz app en: Fix Typo
    
    * Quiz app: Update package-lock.json
    
    * Quiz app zh-cn: Improve.
    
    * Quiz app zh-cn: Delete meaningless ：
    
    * Quiz app zh-cn: Fix typo

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -2,6 +2,7 @@
 import ar from './ar.json';
 import bn from './bn.json';
 import en from './en.json';
+import zh_cn from './zh-cn.json';
 
 //export const defaultLocale = 'en';
 
@@ -9,6 +10,7 @@ const messages = {
 	ar: ar[0],
 	bn: bn[0],
 	en: en[0],
+	"zh-cn": zh_cn[0],
 };
 
 export default messages;
